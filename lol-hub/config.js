2b5fce3809450e14bf8b0528f523d372aa47059f
nachoogm <113169650+nachoogm@users.noreply.github.com>
2026-07-29 19:07:21 +0200

    Update config.js

--- lol-hub/config.js
+++ lol-hub/config.js
@@ -17,8 +17,10 @@ const CONFIG = {
 
   players: [
     { riotId: "Cebolokoh98#SOC",      rol: "Top",     main: "Galio"  },
-    { riotId: "MLVVND El Piruko#EUW",  rol: "Jungla",  main: "Akshan" },
-    { riotId: "ette secht#EUW",        rol: "Mid",     main: "Ahri"   },
+    { riotId: "BARI POPPYNS#2025",      rol: "Top",   main: "Poppy"  },
+    { riotId: "MLVVND El Piruko#EUW",  rol: "Jungla",  main: "Lee sin" },
+    { riotId: "ette secht#EUW",        rol: "Mid",     main: "Akshan"   },
+    { riotId: "Hi Im Gonza#7385",      rol: "Mid",     main: "Fizz"   },
     { riotId: "Parisuko#EUW",          rol: "ADC",     main: "Vayne"  },
     { riotId: "tacua#EUW",             rol: "Support", main: "Lulu"   }
   ]
